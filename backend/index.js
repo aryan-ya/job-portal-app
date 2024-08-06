e1fcef1ccd5db9cb816e31ab8d471b6e257a828c
aryan-ya <ay3978943@gmail.com>
2024-08-06 16:22:17 +0530

    model

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express, { json } from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
 dotenv.config({});
 
 const app = express();
@@ -21,13 +22,15 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
 const corsOptions ={
-    origin: "http/localhost:5173",
+    origin: "http://localhost:5173", // Corrected origin
+
     credentials:true,
 }
 
 app.use(cors(corsOptions));
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
+    connectDB();
     console.log(`Server running at port ${PORT}`);
     
 })
