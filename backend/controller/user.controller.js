c325d562c4eec1ac7f848404eee6b35fa601f066
aryan-ya <ay3978943@gmail.com>
2024-08-07 12:11:59 +0530

    backend

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,6 +1,40 @@
+import {User} from  "../models/user.models.js";
+import bcrypt from  "bcryptjs";
+
 export const register = async (req, res) => {
     try {
-       const  {fullname, email, phonenumber, password, role} = req.body; 
+       const  {fullname, email, phoneNumber, password, role} = req.body; 
+       if (!fullname || !email || !phoneNumber, !password, !role) {
+           return res.status(400).json({
+            message:"Something is missing",
+            success:false
+           });
+       };
+
+       const user= await User.findOne({email});
+       if (user) {
+          return res.status(400).json({
+            message : "User already exists with "
+          })
+       }
+
+       const  hashedPassword = await bcrypt.hash(password, 10);
+       await User.create({
+        fullname,
+        email,
+        phoneNumber,
+        password:hashedPassword,
+        role,
+
+       })
+    } catch (error) {
+        
+    }
+}
+
+export const login = async (req, res) => {
+    try {
+        const {email, password, role} = req.body;
     } catch (error) {
         
     }
